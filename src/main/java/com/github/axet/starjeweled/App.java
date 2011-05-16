c5949bb7e5770947c9e8de490f5094be24cedf76
Alexey Kuznetsov <ak@axet.ru>
2011-05-16 16:54:13 -0300

    add gui

--- src/main/java/com/github/axet/starjeweled/App.java
+++ src/main/java/com/github/axet/starjeweled/App.java
@@ -1,5 +1,6 @@
 package com.github.axet.starjeweled;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Image;
 import java.awt.Point;
@@ -9,9 +10,15 @@ import java.awt.Toolkit;
 import java.awt.event.InputEvent;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.ArrayList;
+import java.util.Date;
 
 import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 
 import com.github.axet.starjeweled.common.Matrix;
 import com.github.axet.starjeweled.common.MoveMatrix;
@@ -73,7 +80,29 @@ public class App {
         user.move(new Point(rectangle.x + rectangle.width + 10, rectangle.y + rectangle.height + 10));
     }
 
+    static public String exceptionString(Throwable e) {
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw);
+        e.printStackTrace(pw);
+        return sw.toString();
+    }
+
     public static void main(String[] args) {
+
+        JFrame frame = new JFrame("Starjeweled");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(new Dimension(600, 300));
+        frame.setAlwaysOnTop(true);
+        frame.getRootPane().setLayout(new BorderLayout());
+
+        JTextArea text = new JTextArea();
+        text.setEditable(false);
+        JScrollPane pScroll = new JScrollPane(text, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
+                JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+
+        frame.getRootPane().add(pScroll, BorderLayout.CENTER);
+
+        frame.setVisible(true);
 
         App app = new App();
 
@@ -86,6 +115,9 @@ public class App {
                 Thread.sleep(1500);
             } catch (Exception e) {
                 e.printStackTrace();
+                
+                text.select(0, 0);
+                text.replaceSelection(new Date() + ": " + exceptionString(e));
 
                 try {
                     Thread.sleep(5000);
