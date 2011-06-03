bb2cab501d56efda92951459af73dba3a740f0d3
Alexey Kuznetsov <ak@axet.ru>
2011-06-03 17:50:16 -0300

    add html output
    Change-Id: Ic0405f902a08b47255df4ae6623fe5f7535f3382

--- src/main/java/com/github/axet/starjeweled/App.java
+++ src/main/java/com/github/axet/starjeweled/App.java
@@ -12,6 +12,7 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -19,6 +20,18 @@ import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.JTextPane;
+import javax.swing.event.HyperlinkEvent;
+import javax.swing.event.HyperlinkListener;
+import javax.swing.text.AttributeSet;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.Document;
+import javax.swing.text.Element;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.html.HTML;
+import javax.swing.text.html.HTML.Tag;
+import javax.swing.text.html.HTMLDocument;
+import javax.swing.text.html.HTMLEditorKit;
 
 import com.github.axet.starjeweled.common.Matrix;
 import com.github.axet.starjeweled.common.MoveMatrix;
@@ -29,118 +42,183 @@ import com.github.axet.starjeweled.common.MoveMatrix;
  */
 
 public class App {
-    Capture capture;
-    Rectangle rectangle;
-    User user;
+	Capture capture;
+	Rectangle rectangle;
+	User user;
 
-    public void init() {
-        user = new User();
-        user.reset();
+	public static class Output implements HyperlinkListener {
+		JTextPane pane;
+		HTMLEditorKit kit = new HTMLEditorKit();
+		HTMLDocument doc = new HTMLDocument();
 
-        capture = new Capture();
-        BufferedImage desktopImage;
-        // desktopImage =
-        // capture.load("/Users/axet/source/starjeweled/starjeweled/big6.png");
-        desktopImage = capture.capture();
-        // capture.write(desktopImage, "capture.png");
+		public Output(JTextPane pane) {
+			pane.setEditorKit(kit);
+			pane.setDocument(doc);
 
-        Lookup lookup = new Lookup();
-        BufferedImage i = lookup.filterMask(desktopImage);
-        // capture.write(i, "mask.png");
-        BufferedImage i2 = lookup.filterNoise(i);
-        // capture.write(i2, "noise.png");
-        rectangle = lookup.getBounds(i2);
-        BufferedImage i3 = lookup.crop(i2, rectangle);
-        // capture.write(i3, "bounds.png");
-    }
+			pane.addHyperlinkListener(this);
 
-    public void run() {
-        BufferedImage desktopRegion = capture.capture(rectangle);
-        // capture.write(desktopRegion, "board.png");
-        Recognition rr = new Recognition(desktopRegion);
+			doc.getStyleSheet().addRule(
+					"A {color:blue;text-decoration:underline}");
 
-        Matrix m = new Matrix(rr);
-        SimpleAI a = new SimpleAI(m);
-        ArrayList<MoveMatrix> ppp = a.getMove();
+		}
 
-        int missClick = 10;
+		public void begin(String html) {
+			try {
+				Element[] roots = doc.getRootElements();
+				Element body = null;
+				for (int i = 0; i < roots[0].getElementCount(); i++) {
+					Element element = roots[0].getElement(i);
+					if (element.getAttributes().getAttribute(
+							StyleConstants.NameAttribute) == HTML.Tag.BODY) {
+						body = element;
+						break;
+					}
+				}
+				kit.insertHTML(doc, body.getStartOffset(), "<pre>" + html
+						+ "</pre>", 0, 0, null);
+			} catch (Exception e) {
+				throw new RuntimeException(e);
+			}
+		}
 
-        for (MoveMatrix p : ppp) {
-            Rectangle mr = rr.getRect(p.p1.x, p.p1.y);
-            Point pp = rr.getMiddle(mr);
-            pp.x += rectangle.x;
-            pp.y += rectangle.y;
-            user.click(user.random(pp, missClick));
+		public void end(String html) {
+			try {
+				kit.insertHTML(doc, doc.getLength(), html, 0, 0, null);
+			} catch (Exception e) {
+				throw new RuntimeException(e);
+			}
+		}
 
-            mr = rr.getRect(p.p2.x, p.p2.y);
-            pp = rr.getMiddle(mr);
-            pp.x += rectangle.x;
-            pp.y += rectangle.y;
-            user.click(user.random(pp, missClick));
-        }
+		public void hyperlinkUpdate(HyperlinkEvent arg0) {
+			try {
 
-        Point home = new Point(rectangle.x + rectangle.width + 10, rectangle.y + rectangle.height + 10);
-        user.move(user.random(home, missClick));
-    }
+				if (arg0.getEventType() == HyperlinkEvent.EventType.ACTIVATED) {
+					java.awt.Desktop.getDesktop().browse(
+							new URI(arg0.getURL().toString()));
+				}
+			} catch (Exception e) {
+				throw new RuntimeException(e);
+			}
+		}
+	}
 
-    static public String exceptionString(Throwable e) {
-        StringWriter sw = new StringWriter();
-        PrintWriter pw = new PrintWriter(sw);
-        e.printStackTrace(pw);
-        return sw.toString();
-    }
+	public void init() {
+		user = new User();
+		user.reset();
 
-    static public void exception(App app, JTextArea text, Exception e, int time) {
-        e.printStackTrace();
+		capture = new Capture();
+		BufferedImage desktopImage;
+		// desktopImage =
+		// capture.load("/Users/axet/source/starjeweled/starjeweled/big6.png");
+		desktopImage = capture.capture();
+		// capture.write(desktopImage, "capture.png");
 
-        text.select(0, 0);
-        text.replaceSelection(new Date() + ": " + exceptionString(e));
+		Lookup lookup = new Lookup();
+		BufferedImage i = lookup.filterMask(desktopImage);
+		// capture.write(i, "mask.png");
+		BufferedImage i2 = lookup.filterNoise(i);
+		// capture.write(i2, "noise.png");
+		rectangle = lookup.getBounds(i2);
+		BufferedImage i3 = lookup.crop(i2, rectangle);
+		// capture.write(i3, "bounds.png");
+	}
 
-        try {
-            Thread.sleep(time);
-        } catch (Exception ignore) {
-        }
-    }
+	public void run() {
+		BufferedImage desktopRegion = capture.capture(rectangle);
+		// capture.write(desktopRegion, "board.png");
+		Recognition rr = new Recognition(desktopRegion);
 
-    public static void main(String[] args) {
+		Matrix m = new Matrix(rr);
+		SimpleAI a = new SimpleAI(m);
+		ArrayList<MoveMatrix> ppp = a.getMove();
 
-        JFrame frame = new JFrame("Starjeweled");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(new Dimension(600, 300));
-        frame.setAlwaysOnTop(true);
-        frame.getRootPane().setLayout(new BorderLayout());
+		int missClick = 10;
 
-        JTextArea text = new JTextArea();
-        text.setEditable(false);
-        JScrollPane pScroll = new JScrollPane(text, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
-                JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+		for (MoveMatrix p : ppp) {
+			Rectangle mr = rr.getRect(p.p1.x, p.p1.y);
+			Point pp = rr.getMiddle(mr);
+			pp.x += rectangle.x;
+			pp.y += rectangle.y;
+			user.click(user.random(pp, missClick));
 
-        frame.getRootPane().add(pScroll, BorderLayout.CENTER);
+			mr = rr.getRect(p.p2.x, p.p2.y);
+			pp = rr.getMiddle(mr);
+			pp.x += rectangle.x;
+			pp.y += rectangle.y;
+			user.click(user.random(pp, missClick));
+		}
 
-        frame.setVisible(true);
+		Point home = new Point(rectangle.x + rectangle.width + 10, rectangle.y
+				+ rectangle.height + 10);
+		user.move(user.random(home, missClick));
+	}
 
-        App app = new App();
+	static public String exceptionString(Throwable e) {
+		StringWriter sw = new StringWriter();
+		PrintWriter pw = new PrintWriter(sw);
+		e.printStackTrace(pw);
+		return sw.toString();
+	}
 
-        boolean reinit = true;
-        while (true) {
-            try {
-                if (reinit)
-                    app.init();
-                app.run();
-            } catch (Lookup.NotFound e) {
-                exception(app, text, e, 5000);
-                reinit = true;
-            } catch (User.MouseMove e) {
-                exception(app, text, e, 5000);
-            } catch (Matrix.UnknownColorFlood e) {
-                exception(app, text, e, 1000);
-                reinit = true;
-            } catch (Recognition.UnknownColor e) {
-                exception(app, text, e, 1000);
-            } catch (Exception e) {
-                exception(app, text, e, 1000);
-            }
-        }
-    }
+	static public void exception(App app, Output out, Exception e, int time) {
+		e.printStackTrace();
 
+		out.begin(new Date() + ": " + exceptionString(e));
+
+		try {
+			Thread.sleep(time);
+		} catch (Exception ignore) {
+		}
+	}
+
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("Starjeweled");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(new Dimension(600, 300));
+		frame.setAlwaysOnTop(true);
+		frame.getRootPane().setLayout(new BorderLayout());
+
+		JTextPane text = new JTextPane();
+		Output out = new Output(text);
+		text.setEditable(false);
+
+		JScrollPane pScroll = new JScrollPane(text,
+				JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
+				JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+
+		frame.getRootPane().add(pScroll, BorderLayout.CENTER);
+
+		out.end("<br/>");
+		out.end("Welcome to Starjweled screen analyser");
+		out.end("<br/>");
+		out.end("If you wana know how this script working please goto:<br/><a href='https://github.com/axet/starjeweled/wiki/Analyse'>https://github.com/axet/starjeweled/wiki/Analyse</a>");
+		out.end("<br/>");
+		out.end("If you have some problems please visit Bugs FAQ page:<br/><a href='https://github.com/axet/starjeweled/wiki/Bugs'>https://github.com/axet/starjeweled/wiki/Bugs</a>");
+		out.end("<br/>");
+
+		frame.setVisible(true);
+
+		App app = new App();
+
+		boolean reinit = true;
+		while (true) {
+			try {
+				if (reinit)
+					app.init();
+				app.run();
+			} catch (Lookup.NotFound e) {
+				exception(app, out, e, 5000);
+				reinit = true;
+			} catch (User.MouseMove e) {
+				exception(app, out, e, 5000);
+			} catch (Matrix.UnknownColorFlood e) {
+				exception(app, out, e, 1000);
+				reinit = true;
+			} catch (Recognition.UnknownColor e) {
+				exception(app, out, e, 1000);
+			} catch (Exception e) {
+				exception(app, out, e, 1000);
+			}
+		}
+	}
 }
