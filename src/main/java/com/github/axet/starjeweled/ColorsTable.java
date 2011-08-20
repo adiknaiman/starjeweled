49e5c2e7393e1e1b7cc4335305c1338a06cb7ff7
Alexey Kuznetsov <ak@axet.ru>
2011-08-20 08:56:23 +0400

    reformat

--- src/main/java/com/github/axet/starjeweled/ColorsTable.java
+++ src/main/java/com/github/axet/starjeweled/ColorsTable.java
@@ -9,6 +9,8 @@ public class ColorsTable {
     public static final RangeColor TABLE_BLACK = new RangeColor(0x121212, 0x181613);
 
     // test colors
-//    public static final RangeColor TABLE_WHITE = new RangeColor(0x2c2c2b, 0x33302b);
-//    public static final RangeColor TABLE_BLACK = new RangeColor(0x191918, 0x201d18);
+    // public static final RangeColor TABLE_WHITE = new RangeColor(0x2c2c2b,
+    // 0x33302b);
+    // public static final RangeColor TABLE_BLACK = new RangeColor(0x191918,
+    // 0x201d18);
 }
