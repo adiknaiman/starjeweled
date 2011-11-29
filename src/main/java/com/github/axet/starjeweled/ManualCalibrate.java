097c1a70a77b1e4cdfd9cb388bc8b7ae40a4f3c3
Alexey Kuznetsov <ak@axet.ru>
2011-11-30 01:00:33 +0400

    add manual calibrate java

---
+++ src/main/java/com/github/axet/starjeweled/ManualCalibrate.java
@@ -0,0 +1,16 @@
+package com.github.axet.starjeweled;
+
+import java.awt.image.BufferedImage;
+
+import com.github.axet.starjeweled.core.Capture;
+
+public class ManualCalibrate {
+
+    public static void main(String[] args) {
+    	Capture capture = new Capture();
+        BufferedImage desktopImage;
+        desktopImage = capture.capture();
+        capture.write(desktopImage, "calibrate.png");
+    }
+    
+}
